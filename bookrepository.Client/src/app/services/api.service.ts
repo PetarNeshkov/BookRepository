c2bb2c67217a9f99bfc1a3744b405ff9c707143b
Петър Нешков <petio567@abv.bg>
2024-10-07 00:57:48 +0300

    Created apiService to call the api

---
+++ bookrepository.Client/src/app/services/api.service.ts
@@ -0,0 +1,52 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService
+{
+  constructor (private http : HttpClient)
+  {
+  }
+
+  get (path : string, params ?: HttpParams) : Observable<any>
+  {
+    return this.http
+      .get(`${ path }`, {params: params})
+      .pipe(catchError(this.formatErrors));
+  }
+
+  put (path : string, body : Object = {}) : Observable<any>
+  {
+    return this.http
+      .put(`${ path }`, body)
+      .pipe(catchError(this.formatErrors));
+  }
+
+  post (path : string, body : Object = {}) : Observable<any>
+  {
+    return this.http
+      .post(`${ path }`, body)
+      .pipe(catchError(this.formatErrors));
+  }
+
+  delete (path : string, params ?: URLSearchParams) : Observable<any>
+  {
+    return params
+      ? this.http
+        .delete(`${ path }?${ params.toString() }`)
+        .pipe(catchError(this.formatErrors))
+      : this.http
+        .delete(path)
+        .pipe(catchError(this.formatErrors));
+  }
+
+  private formatErrors (error : any) : Observable<never>
+  {
+    return throwError(() => error.error);
+  }
+}
