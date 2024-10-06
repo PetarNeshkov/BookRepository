53e8d48a3e60228206a4a334cf98851d13f16837
Петър Нешков <petio567@abv.bg>
2024-10-06 22:23:33 +0300

    Improved program.cs

---
+++ BookRepository.Server/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using BookRepository.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookRepository.Api.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDatabase(
+            this IServiceCollection services,
+            IConfiguration configuration)
+            => services
+                .AddDbContext<BookRepositoryDbContext>(options => options
+                    .UseSqlServer(configuration.GetConnectionString("BookRepositoryConnectionString")));
+
+
+
+        public static IApplicationBuilder MigrateDatabase(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            var timeout = configuration.GetValue("MigrationsDbTimeoutInSeconds", 60);
+
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<BookRepositoryDbContext>();
+
+            dbContext.Database.SetCommandTimeout(TimeSpan.FromSeconds(timeout));
+            dbContext.Database.Migrate();
+
+            return app;
+        }
+
+        public static IApplicationBuilder UseAnyCors(
+           this IApplicationBuilder app)
+           => app
+               .UseCors(options => options
+                   .AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader());
+    }
+}
--- BookRepository.Server/Program.cs
+++ BookRepository.Server/Program.cs
@@ -1,30 +1,28 @@
+using BookRepository.Api.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services
+    .AddDatabase(builder.Configuration)
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddControllers();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app
+    .MigrateDatabase(builder.Configuration)
+    .UseHttpsRedirection()
+    .UseAnyCors()
+    .UseAuthorization();
 
 app.MapControllers();
-
 app.MapFallbackToFile("/index.html");
 
 app.Run();
