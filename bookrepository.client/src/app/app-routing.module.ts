8ef45b5a07e3b78cd6d155ab7af6b7256fee408f
Петър Нешков <petio567@abv.bg>
2024-10-06 21:27:01 +0300

    Added fe side with app-component routing

--- bookrepository.client/src/app/app-routing.module.ts
+++ bookrepository.client/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ListBooksComponent} from './pages/books/list-books/list-books.component';
+import {ManageBookComponent} from './pages/books/manage-book/manage-book.component';
+import {ListAuthorsComponent} from './pages/authors/list-authors/list-authors.component';
+import {ManageAuthorComponent} from './pages/authors/manage-author/manage-author.component';
+import {BookHistoryComponent} from './pages/books/book-history/book-history.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes : Routes = [
+  {path: '', redirectTo: 'books/list', pathMatch: 'full'}, // Redirect root to list-books
+  {path: 'home', redirectTo: 'books/list', pathMatch: 'full'}, // Redirect /home to list-books
+  {
+    path: 'authors', children: [
+      {path: 'list', component: ListAuthorsComponent},
+      {path: 'manage', component: ManageAuthorComponent},
+    ]
+  },
+  {
+    path: 'books', children: [
+      {path: 'list', component: ListBooksComponent}, // Main page for books listing
+      {path: 'manage', component: ManageBookComponent},
+    ]
+  },
+  {path: 'book-history', component: BookHistoryComponent},
+  {path: '**', component: NotFoundComponent} // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bookrepository.client/src/app/app.module.ts
+++ bookrepository.client/src/app/app.module.ts
@@ -1,17 +1,39 @@
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ManageAuthorComponent} from './pages/authors/manage-author/manage-author.component';
+import {ListAuthorsComponent} from './pages/authors/list-authors/list-authors.component';
+import {ManageBookComponent} from './pages/books/manage-book/manage-book.component';
+import {ListBooksComponent} from './pages/books/list-books/list-books.component';
+import {BookHistoryComponent} from './pages/books/book-history/book-history.component';
+import {NotFoundComponent} from './pages/not-found/not-found.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ManageAuthorComponent,
+    ListAuthorsComponent,
+    ManageBookComponent,
+    ListBooksComponent,
+    BookHistoryComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
-    AppRoutingModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
