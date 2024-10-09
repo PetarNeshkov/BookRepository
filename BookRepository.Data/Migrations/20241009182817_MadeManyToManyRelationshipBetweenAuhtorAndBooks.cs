733d57ba4128562776544200a86f1a79c878a6c1
Петър Нешков <petio567@abv.bg>
2024-10-09 22:09:05 +0300

    Improved relationship models

---
+++ BookRepository.Data/Migrations/20241009182817_MadeManyToManyRelationshipBetweenAuhtorAndBooks.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookRepository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MadeManyToManyRelationshipBetweenAuhtorAndBooks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Authors_Books_BookId",
+                table: "Authors");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Authors_BookId",
+                table: "Authors");
+
+            migrationBuilder.DropColumn(
+                name: "BookId",
+                table: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ChangeDescription",
+                table: "BookChanges",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1000)",
+                oldMaxLength: 1000);
+
+            migrationBuilder.CreateTable(
+                name: "AuthorBook",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<int>(type: "int", nullable: false),
+                    BooksId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBook", x => new { x.AuthorsId, x.BooksId });
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBook_Books_BooksId",
+                        column: x => x.BooksId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBook_BooksId",
+                table: "AuthorBook",
+                column: "BooksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBook");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ChangeDescription",
+                table: "BookChanges",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookId",
+                table: "Authors",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_BookId",
+                table: "Authors",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Authors_Books_BookId",
+                table: "Authors",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "Id");
+        }
+    }
+}
